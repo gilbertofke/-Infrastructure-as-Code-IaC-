0b17d9947f7d5d92580efb4077bc4ff96eea7df6
Gilbert Cheruiyot <gilbertofke@gmail.com>
2024-12-02 13:16:22 +0300

    Updates aws instance resource

--- main.tf
+++ main.tf
@@ -1,25 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 # Define the VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-
+  
   tags = {
     Name = "main-vpc"
   }
 }
 
-# Define the subnet
+# Define the subnet in an acceptable availability zone (e.g., us-east-1a)
 resource "aws_subnet" "subnet" {
-  vpc_id                  = aws_vpc.main.id  
+  vpc_id                  = aws_vpc.main.id  # Ensure the subnet is in the same VPC
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"  
+  availability_zone       = "us-east-1a"  # Use an acceptable availability zone
+
   tags = {
     Name = "main-subnet"
   }
 }
 
-# Define the security group
+# Define the security group in the same VPC as the subnet
 resource "aws_security_group" "sg" {
-  vpc_id = aws_vpc.main.id  
+  vpc_id = aws_vpc.main.id  # Ensure the security group is in the same VPC as the subnet
 
   ingress {
     from_port   = 22
@@ -40,9 +45,9 @@ resource "aws_security_group" "sg" {
   }
 }
 
-# Define the EC2 instance
+# Define the EC2 instance in the same subnet and security group
 resource "aws_instance" "vm" {
-  ami               = "ami-0c02fb55956c7d316"
+  ami               = "ami-08eb150f611ca277f" 
   instance_type     = "t2.micro"
   subnet_id         = aws_subnet.subnet.id
   vpc_security_group_ids = [aws_security_group.sg.id]
